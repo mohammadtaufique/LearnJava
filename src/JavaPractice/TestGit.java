f18bd7995a73800e993d65e6833f916dfa0e8d37
Mohammad Taufique <mohammadtaufique45@gmail.com>
2023-04-18 14:48:13 +0530

    Testing local 01 branch in git

--- src/JavaPractice/TestGit.java
+++ src/JavaPractice/TestGit.java
@@ -4,5 +4,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("Testing Git");
         System.out.println("Testing branch in git");
+        System.out.println("Testing local 01 branch in git");
     }
 }
